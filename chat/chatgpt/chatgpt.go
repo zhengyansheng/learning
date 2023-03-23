220539406917162322e3c35add361139389aa7fb
zhengyansheng <zhengyansheng@huohua.cn>
2023-03-23 11:21:11 +0800

    rm bucket

--- chat/chatgpt/chatgpt.go
+++ chat/chatgpt/chatgpt.go
@@ -8,6 +8,10 @@ import (
 	openai "github.com/sashabaranov/go-openai"
 )
 
+var (
+	sqlStatementOptimization = "优化建议：1. 当只要一行数据时使用 LIMIT 1, 2. 为搜索字段建索引, 3. 在Join表的时候使用相当类型的例，并将其索引, 4. 千万不要 ORDER BY RAND(), 5. 避免 SELECT *, 6. 永远为每张表设置一个ID, 7. 使用 ENUM 而不是 VARCHAR, 8. 对查询进行优化，要尽量避免全表扫描, 9. 应尽量避免在 where 子句中对字段进行 null 值判断, 10. 应尽量避免在 where 子句中使用 != 或 <> 操作符, 11. 应尽量避免在 where 子句中使用 or 来连接条件，否则系统将进行全表扫描, 12. 模糊查询前边不要使用%，否则系统将可能无法正确使用索引, 13. 应尽量避免在where子句中对字段进行函数操作，否则系统将可能无法正确使用索引, 14. 不要在 where 子句中的=左边进行函数、算术运算或其他表达式运算，否则系统将可能无法正确使用索引, 15. 不带条件的查询是全表扫描，不允许发起, 16. 尽量避免大事务操作，提高系统并发能力"
+)
+
 type chatGPT struct {
 	client  *openai.Client
 	model   string
@@ -33,11 +37,38 @@ func WithTimeout(timeout int) Option {
 func DefaultSystemChatCompletionMessage() openai.ChatCompletionMessage {
 	return openai.ChatCompletionMessage{
 		Role:    openai.ChatMessageRoleSystem,
-		Content: "你是一个AI助手，我需要你模拟一名拥有15年经验的DBA工程师来回答我的问题",
+		Content: "你是一个乐于助人的AI助手，我需要你模拟一位拥有15年经验的MySQL工程师来回答我的问题",
 	}
 }
 
-type ChatResponse struct {
+// DefaultSQLChatCompletionMessage MySQL optimization
+func DefaultSQLChatCompletionMessage() []openai.ChatCompletionMessage {
+	return []openai.ChatCompletionMessage{
+		{
+			Role:    openai.ChatMessageRoleSystem,
+			Content: "你是一个乐于助人的AI助手，我需要你模拟一位拥有15年经验的MySQL工程师来回答我的问题",
+		},
+		{
+			Role:    openai.ChatMessageRoleUser,
+			Content: "你是谁?",
+		},
+		{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: "我是一名AI助手，拥有15年经验的MySQL工程师，很高兴为你服务，请你说出你的SQL语句，我将要优化，提建议和优化后的SQL语句",
+		},
+		{
+			Role:    openai.ChatMessageRoleUser,
+			Content: sqlStatementOptimization,
+		},
+		{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: "我已经学会了SQL优化标准，接下来SQL语句我将按照以上标准执行优化, 建议和提供优化后的SQL",
+		},
+	}
+
+}
+
+type Response struct {
 	Reply   string                         `json:"reply"`   // 回复
 	Context []openai.ChatCompletionMessage `json:"context"` // 上下文
 }
@@ -54,7 +85,8 @@ func NewGPT(apiAuthTokens []string, opts ...Option) *chatGPT {
 	return chat
 }
 
-func (c *chatGPT) Request(messages []openai.ChatCompletionMessage) (ChatResponse, error) {
+// Request /chat/completions
+func (c *chatGPT) Request(messages []openai.ChatCompletionMessage) (Response, error) {
 	var newMessages []openai.ChatCompletionMessage
 
 	ctx, cancel := context.WithTimeout(context.Background(), c.timeout)
@@ -66,22 +98,26 @@ func (c *chatGPT) Request(messages []openai.ChatCompletionMessage) (ChatResponse
 
 	resp, err := c.client.CreateChatCompletion(ctx,
 		openai.ChatCompletionRequest{
-			Model:    c.model,
-			Messages: messages,
+			Model:            c.model,
+			Messages:         messages,
+			PresencePenalty:  1, // 默认值是0, 惩罚机制，默认是-2.0 到2.0 之间, 数值越小提交重复令牌数越多，从而能更清楚文本的意思
+			FrequencyPenalty: 1, // 默认值是0, 频率，默认是-2.0 到2.0 之间
+			Temperature:      1, // 默认值是1, 温度，默认是0-2，调整回复的精确度使用
+			N:                1, // 默认条数
 		},
 	)
 
 	if err != nil {
-		return ChatResponse{}, err
+		return Response{}, err
 	}
 
-	return ChatResponse{
+	return Response{
 		Reply:   resp.Choices[0].Message.Content,
 		Context: append(newMessages, resp.Choices[0].Message),
 	}, nil
 }
 
-func (c *chatGPT) SimpleRequest(content string) (ChatResponse, error) {
+func (c *chatGPT) SimpleRequest(content string) (Response, error) {
 	messages := []openai.ChatCompletionMessage{
 		{
 			Role:    openai.ChatMessageRoleUser,
--- chat/chatgpt/chatgpt_test.go
+++ chat/chatgpt/chatgpt_test.go
@@ -7,42 +7,48 @@ import (
 )
 
 var (
-	sqlContent = "优化sql语句并提出建议: select id, code, `type`, biz_type, category, user_id, student_id, user_shipping_address_id, country_id, country_name, province_id, province_name, city_id, city_name, area_id, area_name, address, receive_contact_name, receive_contact_phone, receive_phone_code, receive_phone_country_code, pay_account_id, amounts_payable, amounts_actuallypay, create_time, order_status, pay_status, waybill_number, express_order_id, logistics_company, send_time, business_region, business_type, ware_house_country_id from mall_order WHERE ( `type` not in ( 2 , 3 ) and create_time <= '2023-03-12 00:05:00.008' and order_status = 0 )"
+	sqlOptimization = "优化建议：1. 当只要一行数据时使用 LIMIT 1, 2. 为搜索字段建索引, 3. 在Join表的时候使用相当类型的例，并将其索引, 4. 千万不要 ORDER BY RAND(), 5. 避免 SELECT *, 6. 永远为每张表设置一个ID, 7. 使用 ENUM 而不是 VARCHAR, 8. 对查询进行优化，要尽量避免全表扫描, 9. 应尽量避免在 where 子句中对字段进行 null 值判断, 10. 应尽量避免在 where 子句中使用 != 或 <> 操作符, 11. 应尽量避免在 where 子句中使用 or 来连接条件，否则系统将进行全表扫描, 12. 模糊查询前边不要使用%，否则系统将可能无法正确使用索引, 13. 应尽量避免在where子句中对字段进行函数操作，否则系统将可能无法正确使用索引, 14. 不要在 where 子句中的=左边进行函数、算术运算或其他表达式运算，否则系统将可能无法正确使用索引, 15. 不带条件的查询是全表扫描，不允许发起, 16. 尽量避免大事务操作，提高系统并发能力"
+
+	sqlContent = "按照上面的优化建议，优化这条sql语句并提出建议: select id, code, `type`, biz_type, category, user_id, student_id, user_shipping_address_id, country_id, country_name, province_id, province_name, city_id, city_name, area_id, area_name, address, receive_contact_name, receive_contact_phone, receive_phone_code, receive_phone_country_code, pay_account_id, amounts_payable, amounts_actuallypay, create_time, order_status, pay_status, waybill_number, express_order_id, logistics_company, send_time, business_region, business_type, ware_house_country_id from mall_order WHERE ( `type` not in ( 2 , 3 ) and create_time <= '2023-03-12 00:05:00.008' and order_status = 0 )"
 )
 
 func TestChatGPTSimple(t *testing.T) {
 	apiAuthTokens := []string{
-		"",
+		"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",
 	}
 	g := NewGPT(apiAuthTokens, WithModel(openai.GPT3Dot5Turbo0301), WithTimeout(60))
 	response, err := g.SimpleRequest(sqlContent)
 	if err != nil {
 		panic(err)
 	}
-	t.Logf("response: %+v\n", response)
+	t.Logf("response: %+v\n", response.Reply)
 }
 
 func TestChatGPTRequest(t *testing.T) {
 	apiAuthTokens := []string{
-		"",
+		"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",
 	}
 	var newMessages = []openai.ChatCompletionMessage{
 		DefaultSystemChatCompletionMessage(),
 		{
 			Role:    openai.ChatMessageRoleUser,
-			Content: "hello",
-		},
-		{
-			Role:    openai.ChatMessageRoleUser,
-			Content: "hello",
+			Content: "你是谁?",
 		},
 		{
 			Role:    openai.ChatMessageRoleAssistant,
-			Content: "Hi 亲爱的，有什么问题需要我回答吗？",
+			Content: "我是一名AI助手，拥有15年经验的MySQL工程师，很高兴为你服务，请你说出你的SQL语句，我将要优化，提建议和优化后的SQL语句",
 		},
 		{
 			Role:    openai.ChatMessageRoleUser,
-			Content: "golang 实现快速排序",
+			Content: sqlOptimization,
+		},
+		{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: "我已经学会了SQL优化标准，接下来SQL语句我将按照以上标准执行优化, 建议和提供优化后的SQL",
+		},
+		{
+			Role:    openai.ChatMessageRoleUser,
+			Content: sqlContent,
 		},
 	}
 
