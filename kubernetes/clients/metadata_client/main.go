998ebb0eb29f6f968506700e1b89628651ee8867
zhengyansheng <zhengyansheng@vipkid.com.cn>
2023-07-30 18:52:29 +0800

    metadataclient delete

--- kubernetes/clients/metadata_client/main.go
+++ kubernetes/clients/metadata_client/main.go
@@ -18,7 +18,7 @@ var (
 	defaultName = "terraform-example"
 )
 
-func main() {
+func getMetadataClient() metadata.Interface {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -36,6 +36,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	return metadataClient
+}
+
+func GetRs() {
+	metadataClient := getMetadataClient()
+
 	// 通过metadata找到资源对应的owner
 	resource := schema.GroupVersionResource{Group: "apps", Version: "v1", Resource: "replicasets"}
 	owner, err := metadataClient.Resource(resource).Namespace("default").Get(context.TODO(), "nginx-dep2-695468c58d", metav1.GetOptions{})
@@ -46,4 +52,20 @@ func main() {
 	fmt.Printf("owner references: %+v\n", owner.GetOwnerReferences())
 	ownerAccessor, err := meta.Accessor(owner)
 	fmt.Println(ownerAccessor.GetDeletionTimestamp())
+
+}
+
+func DeletePod() {
+	metadataClient := getMetadataClient()
+
+	// 通过metadata找到资源对应的owner
+	resource := schema.GroupVersionResource{Group: "", Version: "v1", Resource: "pods"}
+	err := metadataClient.Resource(resource).Namespace("default").Delete(context.TODO(), "nginx2", metav1.DeleteOptions{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	DeletePod()
 }
