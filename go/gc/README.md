d121f6b7dd0ec557c24c63dd444ac9df65a2db0a
zhengyansheng <zhengyansheng@vipkid.com.cn>
2024-09-09 11:50:41 +0800

    删除html

---
+++ go/gc/README.md
@@ -0,0 +1,11 @@
+# gc
+
+```bash
+#  GODEBUG='gctrace=1' go test -v -run TestForceGC .
+```
+
+
+```bash
+# go test -v -run TestTraceFile .
+# go tool trace trace.out 
+```
--- go/gc/gc_test.go
+++ go/gc/gc_test.go
@@ -1,8 +1,13 @@
 package gc
 
 import (
+	"log"
+	"os"
 	"runtime"
+	"runtime/debug"
+	"runtime/trace"
 	"testing"
+	"time"
 )
 
 func TestForceGC(t *testing.T) {
@@ -15,4 +20,62 @@ func TestForceGC(t *testing.T) {
 
 	var forcegcperiod int64 = 2 * 60 * 1e9
 	t.Log(forcegcperiod)
+}
+
+func TestGcTrace(t *testing.T) {
+	start := time.Now()
+	for i := 0; i < 100000; i++ {
+		_ = make([]byte, 1<<20)
+	}
+	t.Logf("interval: %v", time.Since(start))
+}
+
+func TestSlice(t *testing.T) {
+	start := time.Now()
+	//var arr []int
+	arr := make([]int, 1000)
+	for i := 0; i < 100000; i++ {
+		arr = append(arr, i)
+	}
+	t.Logf("interval: %v", time.Since(start))
+}
+
+// TestTraceFile go tool trace
+func TestTraceFile(t *testing.T) {
+	f, err := os.Create("trace.out")
+	if err != nil {
+		t.Failed()
+	}
+	defer f.Close()
+
+	trace.Start(f)
+	defer trace.Stop()
+	for i := 0; i < 100000; i++ {
+		_ = make([]byte, 1<<20)
+	}
+
+}
+func printGCStats() {
+	t := time.NewTicker(time.Second)
+	s := debug.GCStats{}
+	m := runtime.MemStats{}
+	for {
+		select {
+		case <-t.C:
+			debug.ReadGCStats(&s)
+			log.Printf("gc总次数: %d, 上次gc时间: %v, pauseTotal: %v", s.NumGC, s.LastGC, s.PauseTotal)
+			runtime.ReadMemStats(&m)
+			log.Printf("gc总次数: %d, 上次gc时间: %v, heap_obj_num: %v", m.NumGC, time.Unix(int64(time.Duration(m.LastGC).Seconds()), 0), m.HeapObjects)
+		}
+	}
+}
+
+func TestSlice2(t *testing.T) {
+	go printGCStats()
+	start := time.Now()
+	var arr []int
+	for i := 0; i < 10000000000; i++ {
+		arr = append(arr, i)
+	}
+	t.Logf("interval: %v", time.Since(start))
 }
